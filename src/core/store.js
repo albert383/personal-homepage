047fd52df8b83396437290ea3c300f06b9396ea3
Albert Ciesla <albert383@gmail.com>
2024-11-08 19:56:37 +0100

    Configure Redux store with theme and personalHomepage reducers

--- src/core/store.js
+++ src/core/store.js
@@ -1,18 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
-import rootSaga from './saga';
+import personalHomepageReducer from '../features/personalHomepage/personalHomepageSlice';
 import themeReducer from '../common/themeSlice';
+import saga from './saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
+        personalHomepage: personalHomepageReducer,
         theme: themeReducer,
     },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
+    middleware: [sagaMiddleware],
 });
 
-sagaMiddleware.run(rootSaga);
+sagaMiddleware.run(saga);
 
 export default store;
