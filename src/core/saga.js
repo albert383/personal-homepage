329381b4e5733679364ccda4a04731c9124a4704
Albert Ciesla <albert383@gmail.com>
2024-11-12 20:19:17 +0100

    personalHomepageSaga new import path

--- src/core/saga.js
+++ src/core/saga.js
@@ -1,5 +1,5 @@
 import { all } from 'redux-saga/effects';
-import personalHomepageSaga from '../features/personalHomepage/personalHomepageSaga';
+import personalHomepageSaga from "../features/PersonalHomepage/personalHomepageSaga";
 
 export default function* rootSaga() {
     yield all([
