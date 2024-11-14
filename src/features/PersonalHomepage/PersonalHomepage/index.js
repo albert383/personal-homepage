64f68bf7ca3a993620a53cb5ed1eb43a1bfa951c
Albert Ciesla <albert383@gmail.com>
2024-11-14 17:43:11 +0100

    Add main Skills sections to PersonalHomepage layout

--- src/features/PersonalHomepage/PersonalHomepage/index.js
+++ src/features/PersonalHomepage/PersonalHomepage/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { MainInfo } from './MainInfo';
 import ThemeSwitch from '../../../common/ThemeSwitch';
 import { Container } from './styled';
+import { Skills } from './Skills';
+import { nextSkills, skills } from './Skills/skillsData';
 
 export const PersonalHomepage = () => (
     <Container>
         <ThemeSwitch />
         <MainInfo />
+        <main>
+            <Skills title={"My skillset includes 🛠️"} skills={skills} />
+            <Skills title={"What I want to learn next 🚀"} skills={nextSkills} />
+        </main>
     </Container>
 );
