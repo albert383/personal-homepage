faa8e1579c127209b03f8cd0093d48a8c5d8d83f
Albert Ciesla <albert383@gmail.com>
2024-11-14 17:45:43 +0100

    Add file with skills and nextSkills arrays

---
+++ src/features/PersonalHomepage/PersonalHomepage/Skills/skillsData.js
@@ -0,0 +1,64 @@
+export const skills = [
+    // HTML
+    "Semantic & accessible HTML",
+
+    // CSS
+    "CSS Flexbox, Grid",
+    "Media Queries Level 4",
+    "Responsive Web Design",
+    "Animation",
+    "Keyframes",
+    "Normalize.css",
+    "CSS BEM convention",
+    "Styled Components",
+
+    // JavaScript
+    "Java Script ECMAScript 6",
+    "Promises, Async/Await",
+    "Fetch/Axios",
+    "Error handling: Try/Catch",
+    "Immutability",
+
+    // React
+    "React.js",
+    "JSX",
+    "React Hooks",
+    "Custom Hooks",
+    "React Router",
+
+    // Zarządzanie stanem
+    "Redux Toolkit",
+    "Redux-Saga",
+    "Working with API",
+    "LocaleStorage",
+
+    // Budowanie i konfiguracja
+    "Babel",
+    "Webpack",
+    "NPM",
+
+    // Git i zarządzanie kodem
+    "Git",
+    "GitHub Pull Requests & Review",
+
+    // Metodyki pracy i narzędzia
+    "TeamWork",
+    "Scrum",
+    "Figma",
+    "Trello",
+    "Markdown"
+];
+
+export const nextSkills = [
+    "Java",
+    // Narzędzia
+    "TypeScript",
+    "Node.js",
+    "Cypress",
+
+    // CSS Frameworks
+    "Tailwind CSS",
+
+    // Frameworky
+    "Next.js"
+];
