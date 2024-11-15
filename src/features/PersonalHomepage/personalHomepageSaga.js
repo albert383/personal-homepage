f10bbac680828fa353738ab16882399cc03416d2
Albert Ciesla <albert383@gmail.com>
2024-11-15 19:23:06 +0100

    Update personalHomepageSaga.js

--- src/features/PersonalHomepage/personalHomepageSaga.js
+++ src/features/PersonalHomepage/personalHomepageSaga.js
@@ -1,19 +1,19 @@
+import { getRepositories } from './personalHomepageAPI';
 import { takeLatest, call, put, delay } from 'redux-saga/effects';
 import { fetchRepositories, fetchRepositoriesSuccess, fetchRepositoriesError } from './personalHomepageSlice';
-import { getRepositories } from './personalHomepageAPI';
+
 
 const loadingDelay = 2000;
-
 function* fetchRepositoriesHandler({ payload: username }) {
     try {
         yield delay(loadingDelay); // for loading demonstration only
         const repositories = yield call(getRepositories, username);
         yield put(fetchRepositoriesSuccess(repositories));
     } catch (error) {
-        yield put(fetchRepositoriesError());
+        yield put(fetchRepositoriesError(error));
     }
 }
 
 export default function* personalHomepageSaga() {
-    yield takeLatest(fetchRepositories.type, fetchRepositoriesHandler);
+    yield takeLatest(fetchRepositories, fetchRepositoriesHandler);
 }
